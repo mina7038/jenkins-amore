eb0271f812ebe1df415634517c1c39ba22c45a2c
​mina7038 <alsk4325@naver.com>
2025-07-18 09:38:23 +0900

    13

--- src/main/java/com/apgroup/app/config/WebConfig.java
+++ src/main/java/com/apgroup/app/config/WebConfig.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-   @Override
-   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	   registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:C:/uploads/");
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/home/ec2-user/uploads/");
     }
     
     //@Override
